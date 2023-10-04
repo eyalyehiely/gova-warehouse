1d80422abfb03d66a44d7f584d5e191642a67543
eyalyehiely <eyalwork0@gmail.com>
2023-10-05 00:39:00 +0300

    tenth commit

--- app.py
+++ app.py
@@ -92,13 +92,127 @@ def register():
     new_user=request.form['username']
     for user in users_table:
         if new_user == user['username']:
-            return render_template('register.html',users_table=users_table,new_user=new_user,message='This user is already exist')
+            return render_template('register.html')
         
     query(f"INSERT INTO users VALUES('{request.form['name']}' ,'{request.form['username']}' , '{request.form['password']}' , '{request.form['phone']}' , '{request.form['email']}', '{request.form.get('team')}' )")
     
     session['username'] = request.form['username'] 
     return redirect('/')
 
+
+
+#requests
+#----------------------------------------------------------------------------------#
+
+@app.route('/requests',methods = ['POST','GET'])
+def requests():
+    #checking validation & return template
+   for user in users_table:
+        if session.get('username') == user['username']:
+            return render_template('requests.html')
+        else:
+            return redirect('/login')
+        
+def create_request_number():
+    try:
+        starter_number= '#'
+        request_number = None
+        for i in range(10):
+            number = random.randrange(1,10)
+            starter_number+=str(number)
+        request_number= starter_number        
+        return request_number
+    except:
+        if request_number == query(f"SELECT request_number FROM requests WHERE request_number='{request_number}'"):
+            random.shuffle(request_number)
+        return str(request_number)
+
+
+
+
+#checking validation & return template
+@app.route('/add_requests',methods = ['POST','GET'])
+def add_requests():
+   
+   for user in users_table:
+        if session.get('username') == user['username']:
+            return render_template('add_requests.html')
+        else:
+            return redirect('/login')
+        
+
+
+ #add request to db & updates the tables
+@app.route('/insert_requests',methods = ['POST','GET'])
+def insert_requests():
+    try:
+        request_number=create_request_number()
+        username= session.get('username')
+        quantity = request.form.get('quantity')
+        items = request.form['Item']
+        taking_date = request.form.get('taking_date')
+        returning_date =request.form.get('returning_date')
+        status = "not approved"
+        query(f"INSERT INTO requests VALUES('{request_number}', '{username}','{items}', '{quantity}', '{taking_date}', '{returning_date}','{status}','{datetime.datetime.today()}')")
+        query(f"UPDATE items SET quantity_in_stock = quantity_in_stock - '{int(quantity)}' WHERE item_name ='{request.form.get('Item')}'") 
+        return render_template('add_requests.html')   
+    except:
+        return "item not available" 
+
+
+
+#return all items in stock
+@app.route('/send_item_instock', methods= ["GET"])
+def send_item_instock():
+    chosen_item = request.args.get('Item')
+    rows =(query(f"SELECT quantity_in_stock FROM items WHERE item_name='{chosen_item}'"))
+    items_instock =[]
+    for row in rows:
+        items_instock.append({'item_name':row[0],'quantity_in_stock':row[1]})   
+    return jsonify(items_instock)
+
+
+
+#return all items per category
+@app.route('/select_category', methods= ["GET"])
+def add_item_request():
+    chosen_category = request.args.get('category')
+    rows = query(f"SELECT mkt,item_name FROM items WHERE category='{chosen_category}'")
+    items =[]
+    for row in rows:
+        items.append({'mkt':row[0],'item_name':row[1]})   
+    return jsonify(items)
+
+
+
+
+
+
+
+#Exit
+#----------------------------------------------------------------------------------#
+@app.route('/exit')
+def exit():
+    session['username']= None
+    return redirect('/login')
+
+
+
+
+
+#Superuser
+#----------------------------------------------------------------------------------#
+@app.route('/admin')
+def admin():
+    if session.get('username') == 'admin':
+        return render_template('admin.html')
+    else:
+        return render_template('admin_error.html')
+    
+
+
+# - user
+#----------------------------------------------------------------------------------#
 
 #users menu - admin
 @app.route('/admin/users_menu')
@@ -107,24 +221,34 @@ def users_menu():
         return render_template('users_menu.html')
 
 
+
 #delete user
 @app.route('/admin/users_menu/delete',methods = ['GET','POST'])
 def delete_user():
     deleted_user = request.form.get('username')
     query(f"DELETE FROM users WHERE username='{deleted_user}'")
     
-    return render_template('delete_user.html',users_table=users_table,deleted_user=deleted_user,message1 = 'No such user',message2 = 'User deleted')
+    return render_template('delete_user.html')
 
 
-with sqlite3.connect('users.db') as conn: 
-    query1 = "SELECT * FROM users" 
-    df = pandas.read_sql_query(query1, conn)
-    df.to_csv('users.csv', index=False)
-    
-     
 
-#items - add,update,delete,read
-#--------------------------------------------------------------------------------#
+#export users table to excel 
+@app.route('/download_excel_users',methods = ['GET','POST'])
+def excel_users():
+    with sqlite3.connect('users.db') as conn: 
+        query1 = "SELECT * FROM users" 
+        df = pandas.read_sql_query(query1, conn)
+    df.to_excel('users.xlsx',index=False)
+    return send_file(
+        'users.xlsx',
+        as_attachment=True,
+        download_name='users_data.xlsx',
+        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+
+#items
+#-------------------------------------------------------------------------------------#
+
 #items menu
 @app.route('/items')
 def items():
@@ -141,11 +265,10 @@ def get_items():
     if session.get('username') != 'admin':
         return redirect('/')
     
-    for item in items_table:
-        if request.form.get("mkt") == item["mkt"]:
-           message1 = 'item is already exist'
-           return render_template('add_items.html',message1=message1,switch=1)
-        pass
+    # for item in items_table:
+    #     if request.form.get("mkt") == item["mkt"]:
+    #        return render_template('add_items.html')
+        
            
     query(f"INSERT INTO items VALUES('{request.form.get('mkt')}', '{request.form.get('category')}', '{request.form.get('item_name')}', '{request.form.get('quantity')}','{request.form.get('quantity')}' ,'{request.form.get('added_by')}','{request.form.get('entrance_date')}','{datetime.datetime.now()}')")
     return render_template('add_items.html')
@@ -180,99 +303,6 @@ def delete_item():
 
 
 
-
-
-
-
-#requests
-#----------------------------------------------------------------------------------#
-
-@app.route('/requests',methods = ['POST','GET'])
-def requests():
-    #checking validation & return template
-   for user in users_table:
-        if session.get('username') == user['username']:
-            return render_template('requests.html')
-        else:
-            return redirect('/login')
-        
-# def request_number():
-#     serial= '0'
-#     for i in range(10):
-#         number = random.randrange(1,10)
-#         serial+=str(number)
-#     for request in requests_table:
-#         if request['request_number'] == serial:
-#         try:
-            
-#     return serial
-
-
-
-@app.route('/add_requests',methods = ['POST','GET'])
-def add_requests():
-   #checking validation & return template
-   for user in users_table:
-        if session.get('username') == user['username']:
-            return render_template('add_requests.html')
-        else:
-            return redirect('/login')
-        
-
-
-
-@app.route('/insert_requests',methods = ['POST','GET'])
-def insert_requests():
-    #add request to db & updates the tables
-    username= session.get('username')
-    quantity = request.form.get('quantity')
-    items = request.form['Item']
-    taking_date = request.form.get('taking_date')
-    returning_date =request.form.get('returning_date')
-    query(f"INSERT INTO requests VALUES('1', '{username}','{items}', '{quantity}', '{taking_date}', '{returning_date}')")
-    query(f"UPDATE items SET quantity_in_stock = quantity_in_stock - '{int(quantity)}' WHERE item_name ='{request.form.get('Item')}'") 
-    return render_template('add_requests.html')    
-    
-
-
-@app.route('/select_category', methods= ["GET"])
-def add_item_request():
-    #return all items per category
-    chosen_category = request.args.get('category')
-    rows = query(f"SELECT mkt,item_name FROM items WHERE category='{chosen_category}'")
-    items =[]
-    print(rows)
-    for row in rows:
-        items.append({'mkt':row[0],'item_name':row[1]})   
-    return jsonify(items)
-
-
-
-
-
-
-
-#Exit
-#----------------------------------------------------------------------------------#
-@app.route('/exit')
-def exit():
-    session['username']= None
-    return redirect('/login')
-
-
-
-
-
-#superuser
-#----------------------------------------------------------------------------------#
-@app.route('/admin')
-def admin():
-    if session.get('username') == 'admin':
-        return render_template('admin.html')
-    else:
-        return render_template('admin_error.html')
-
-
 #export items table to excel 
 @app.route('/download_file',methods = ['GET','POST'])
 def excel_items():
@@ -288,19 +318,30 @@ def excel_items():
         )
 
 
-#export users table to excel 
-@app.route('/download_excel_users',methods = ['GET','POST'])
-def excel_users():
-    with sqlite3.connect('users.db') as conn: 
-        query1 = "SELECT * FROM users" 
-        df = pandas.read_sql_query(query1, conn)
-    df.to_excel('users.xlsx',index=False)
-    return send_file(
-        'users.xlsx',
-        as_attachment=True,
-        download_name='users_data.xlsx',
-        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
+
+
+
+#requests
+#-------------------------------------------------------------------------------------#
+
+#requests menu
+@app.route('/requests_menu')
+def requests_menu():
+    if session.get('username') != 'admin':
+        return redirect('/')
+    return render_template('requests_menu.html')
+
+
+
+#delete request from db
+@app.route('/requests_menu/delete_requests',methods = ['GET','POST'])
+def delete_request():
+    request_number = request.form.get('request_number')
+    query(f"DELETE FROM requests WHERE request_number='{request_number}'")
+    return render_template('delete_requests.html')
+
+
+
 
 
 
@@ -319,3 +360,4 @@ def excel_requests():
         download_name='requests_data.xlsx',
         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         )
+
